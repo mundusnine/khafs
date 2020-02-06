ac73ee67519795da8855e6b4ec05c8611bd4e79a
jsn213 <jean-sebastien.nadeau.3@ulaval.ca>
2020-02-06 18:49:35 -0500

    Update comlink.d.ts, node-adapter.d.ts, and 10 more files...

--- getDevDeps.sh
+++ getDevDeps.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+version="0.6.0"
+wget -m -A * -pk -e robots=off https://unpkg.com/browse/@wasmer/wasmfs@$version/lib/
+mv unpkg.com/@wasmer/wasmfs@$version/lib/node_modules ./Assets/node_modules
+mv unpkg.com/@wasmer/wasmfs@$version/lib/packages ./Assets/packages
+mv unpkg.com/@wasmer/wasmfs@$version/lib/index.iife.js ./Assets/wasmfs.js
+rm -r unpkg.com
 wget https://unpkg.com/dexie@latest/dist/dexie.js
 mv dexie.js ./Assets/dexie.js
