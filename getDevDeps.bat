0a8b80a942591d3125f5d2d6841f0e572ebe6842
jsn213 <jean-sebastien.nadeau.3@ulaval.ca>
2020-09-01 19:19:35 -0400

    Update dexie and wasmfs

--- getDevDeps.bat
+++ getDevDeps.bat
@@ -1,10 +1,11 @@
 start "" /WAIT cmd.exe /c "scoop install coreutils"
 start "" /WAIT cmd.exe /c "scoop install wget"
-set version="0.11.2"
-wget -m -A * -pk -e robots=off https://unpkg.com/browse/@wasmer/wasmfs@%version%/lib/
+set version=0.11.2
+wget -r -p -k https://unpkg.com/browse/@wasmer/wasmfs@%version%/lib/
 mv unpkg.com/@wasmer/wasmfs@%version%/lib/node_modules ./Assets/node_modules
 mv unpkg.com/@wasmer/wasmfs@%version%/lib/packages ./Assets/packages
 mv unpkg.com/@wasmer/wasmfs@%version%/lib/index.iife.js ./Assets/wasmfs.js
-rm -r unpkg.com
+
 wget https://unpkg.com/dexie@latest/dist/dexie.js
 mv dexie.js ./Assets/dexie.js
+rm -r unpkg.com
