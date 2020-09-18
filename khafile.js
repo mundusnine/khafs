e328c59b4cf96faa652e9b487716411f9c7573ff
mundusnine <mundusnine@gmail.com>
2020-09-18 09:32:33 -0400

    Support both debug-html5 and html5.

--- khafile.js
+++ khafile.js
@@ -3,7 +3,7 @@ project.addAssets('Assets/wasmfs.js');
 project.addAssets('Assets/dexie.js');
 project.addDefine("wasmfs");
 
-let html5 = process.argv.indexOf("html5") >= 0;
+let html5 = process.argv.indexOf("html5") >= 0 || process.argv.indexOf("debug-html5") >= 0;;
 if(html5){
     project.addSources('Sources/khafs');
     project.addSources('Sources/sys');
